b8cb65500478a248adb7dc6c1cc886ea2a5d9c34
My test <dm.karpenko@istu.edu.ua>
2025-02-19 14:13:44 +0200

    2 tables

--- laravel/app/Http/Controllers/ImageController.php
+++ laravel/app/Http/Controllers/ImageController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
-// use App\Http\Controllers\Controller;
 use App\Models\Image;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
-    public function store(Request $request){
+    public function store(Request $request)
+    {
+        $existingImage = Image::where('product_id', $request->product_id)
+            ->where('url', $request->url)
+            ->first();
+
+        if ($existingImage) {
+            return response()->json($existingImage, 200);
+        }
+        
         return Image::create($request->all());
     }
 }
--- laravel/app/Http/Controllers/ProductController.php
+++ laravel/app/Http/Controllers/ProductController.php
@@ -7,14 +7,22 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index () {
+    public function index()
+    {
         return Product::all();
     }
-    public function store (Request $request)
-{
-    // return response(['allData' => $request->all()]);
 
-    return Product::create($request->all());
-}
-}
+    public function store(Request $request)
+    {
+        $existingProduct = Product::where('title', $request->title)
+            ->where('description', $request->description)
+            ->first();
 
+        if ($existingProduct) {
+            return response()->json($existingProduct, 200);
+        }
+        
+        // return response(['allDate' => $request->all()], 200);
+        return Product::create($request->all());
+    }
+}
