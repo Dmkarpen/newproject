a89899aadac68f767ed65735dcc4bf5d583ee16e
My test <dm.karpenko@istu.edu.ua>
2025-03-07 14:09:29 +0200

    new commit - usertoken

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,13 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ImageController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\AuthController;
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/product-create', [ProductController::class, 'store']);
 Route::post('/product-image', [ImageController::class, 'store']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::resource('orders', OrderController::class);
+
+Route::post('/register', [AuthController::class, 'register'])->name('api.register');
+Route::post('/login', [AuthController::class, 'login'])->name('api.login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('api.logout');
+Route::get('/me', [AuthController::class, 'me']);
